52dcc2972836d956894a4f93a53869fedde9727e
Melissa Arrington <melissa.arrington1@yahoo.com>
2019-02-09 14:41:35 -0500

    updated server

--- server.js
+++ server.js
@@ -3,11 +3,19 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(__dirname + '/swoosh/index.html')
+
 });
 
 app.use(bodyParser.json());
 
-app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
-});
+app.use(express.static('swoosh'));
+
+// app.listen(8000, () => {
+//   console.log('Example app listening on port 8000!')
+// });
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log("Magic happening on port " + PORT);
+})
