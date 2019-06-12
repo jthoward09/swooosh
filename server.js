ffd9978f6e5c3ce9cb35a6e87f9458c28af77368
Melissa Arrington <melissa.arrington1@yahoo.com>
2019-06-12 01:08:09 -0400

    herokuu

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/swoosh/index.html')
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/swoosh/index.html");
 });
 
 app.use(bodyParser.json());
-app.use(express.static('swoosh'));
+app.use(express.static(__dirname + "/swoosh/index.html"));
 
 app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
+  console.log("Example app listening on port 8000!");
 });
